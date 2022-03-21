df80a59457d831ea28b20fa6b3e434a8b43027aa
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-03-22 03:57:08 +0800

    Latihan dan Praktikum minggu 4

--- Minggu 4/Praktikum/CI3-upload/application/views/v_upload.php
+++ Minggu 4/Praktikum/CI3-upload/application/views/v_upload.php
@@ -4,7 +4,7 @@
 
 <body>
 	<center>
-		<h1>Membuat Upload File Dengan CodeIgniter | MalasNgoding.com</h1>
+		<h1>Membuat Upload File Dengan CodeIgniter 3 | MalasNgoding.com</h1>
 	</center> <?php echo $error; ?> <?php echo form_open_multipart('upload/aksi_upload'); ?> <input type="file" name="berkas" /> <br /><br /> <input type="submit" value="upload" /> </form>
 </body>
 
