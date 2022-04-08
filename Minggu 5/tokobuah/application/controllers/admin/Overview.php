12f1e67b0ba114e68aa0fe7049bee2abaa2af1ca
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-04-08 15:22:28 +0800

    Latihan dan Praktikum Minggu 5

---
+++ Minggu 5/tokobuah/application/controllers/admin/Overview.php
@@ -0,0 +1,12 @@
+<?php class Overview extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	public function index()
+	{
+		// load view admin/overview.php 
+		$this->load->view("admin/overview");
+	}
+}
